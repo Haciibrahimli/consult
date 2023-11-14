989a429cb3422f9f7fdb5865f4795307b451450a
Haciibrahimli <haci.ibrahimli@gmail.com>
2023-11-14 20:04:46 +0200

    lastchanges

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from my_app.models import Service
+
+from django.shortcuts import redirect, render
+from my_app.models import Blog, Service
+from django.contrib.auth import authenticate, login, logout
+
 
 def service_view(request):
 
@@ -19,6 +22,43 @@ def detail_view(request,slug):
     'obj1':obj1
     }
     return render(request,'service-detail.html',context)
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("pwd")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('service')
+        
+    context = {}
+
+    return render(request, "login.html", context)
+
+def logout_view(request):
+    logout(request)
+    return redirect("service")
+
+def blog_view(request):
+
+    blogs = Blog.objects.all()
+ 
+
+    context = {
+    'blogs':blogs
+    }
+    return render(request,'blog.html',context)
+
+
+def blog_detail_view(request,slug):
+
+    blog = Blog.objects.get(slug=slug)
+
+    context = {
+    'blog':blog
+    }
+    return render(request,'detail.html',context)
 
 
 
