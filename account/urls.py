6bb06bc687844ac6c3f755f03af8202998d98aa3
Haciibrahimli <haci.ibrahimli@gmail.com>
2023-11-22 20:35:49 +0200

    MESA

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,8 @@ from account.views import *
 urlpatterns = [
     path("register/", register_view, name='register'),
     path("activate/<slug>/", activate_view, name='activate'),
+    path("email/", email_view, name='email'),
+    path("forgot/check/<slug>/", forgot_checker, name='forgot_otp'),
+    path("reset/password/<slug>/", reset_password, name='reset_password'),
+
 ]
